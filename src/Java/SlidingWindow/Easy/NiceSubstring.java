e20ad0215babec28fd20b8a0ac2280a516f828f9
SCHOOMP <grahamschomp2@gmail.com>
2025-04-29 18:28:56 -0500

    NiceSubString

--- src/Java/SlidingWindow/Easy/NiceSubstring.java
+++ src/Java/SlidingWindow/Easy/NiceSubstring.java
@@ -1,4 +1,26 @@
 package Java.SlidingWindow.Easy;
 
+import java.util.*;
+
 public class NiceSubstring {
+
+    public static String longestNiceSubstring(String s) {
+            if (s.length() < 2) return "";
+            char[] arr = s.toCharArray();
+            Set<Character> set = new HashSet<>();
+            for (char c : arr) set.add(c);
+            for (int i = 0; i < arr.length; i++) {
+                char c = arr[i];
+                if (set.contains(Character.toUpperCase(c)) && set.contains(Character.toLowerCase(c))) continue;
+                String sub1 = longestNiceSubstring(s.substring(0, i));
+                String sub2 = longestNiceSubstring(s.substring(i + 1));
+                return sub1.length() >= sub2.length() ? sub1 : sub2;
+            }
+            return s;
+        }
+
+    public static void main(String[] args) {
+        String s = "YazaAay";
+        System.out.println("Nice Substring: " + longestNiceSubstring(s));  // Output: "aAa"
+    }
 }

5e1a51cce0d77e6d4f19f81578ab9c38827ea899
SCHOOMP <grahamschomp2@gmail.com>
2025-05-19 15:29:30 -0500

    Create TypeOfTriangle.java

---
+++ src/Java/Array/Easy/TypeOfTriangle.java
@@ -0,0 +1,15 @@
+package Java.Array.Easy;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
+public class TypeOfTriangle {
+
+    public static String triangleType(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,3,3};
+        System.out.println("The type of triangle is : " + triangleType(nums));
+    }
+}

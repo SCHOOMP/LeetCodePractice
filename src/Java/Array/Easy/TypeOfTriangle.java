13d22ee847be6f3e64ab5607f19f7c1422f28a29
SCHOOMP <grahamschomp2@gmail.com>
2025-05-19 15:48:54 -0500

    Answer

--- src/Java/Array/Easy/TypeOfTriangle.java
+++ src/Java/Array/Easy/TypeOfTriangle.java
@@ -1,15 +1,33 @@
 package Java.Array.Easy;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.Arrays;
 
 public class TypeOfTriangle {
 
     public static String triangleType(int[] nums) {
+        if (nums.length != 3) {
+            return "none";
+        }
 
+        Arrays.sort(nums);
+        int a = nums[0], b = nums[1], c = nums[2];
+
+        // Triangle Inequality Theorem
+        if (a + b <= c) {
+            return "none";
+        }
+
+        if (a == b && b == c) {
+            return "equilateral";
+        } else if (a == b || b == c || a == c) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
     }
 
     public static void main(String[] args) {
-        int[] nums = {3,3,3};
+        int[] nums = {8, 4, 4};
         System.out.println("The type of triangle is : " + triangleType(nums));
     }
 }

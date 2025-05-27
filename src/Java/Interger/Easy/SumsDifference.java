3a63b8e811ec99b5378fbf39d3939249c26fa573
SCHOOMP <grahamschomp2@gmail.com>
2025-05-27 09:20:11 -0400

    SumsDiffe

---
+++ src/Java/Interger/Easy/SumsDifference.java
@@ -0,0 +1,24 @@
+package Java.Interger.Easy;
+
+
+public class SumsDifference {
+
+    public static int differenceOfSums(int n, int m) {
+        int solution = 0;
+        for (int i = 1; i < n+1; i++){
+            if(i % m != 0){
+                solution += i;
+            }
+            if (i % m == 0){
+                solution -= i;
+            }
+        }
+        return solution;
+    }
+
+
+    public static void main(String[] args) {
+       int n = 10, m = 3;
+       System.out.println(differenceOfSums(n, m));
+    }
+}
